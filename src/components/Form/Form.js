2aa75b5495a0a1196baca653bc54e622b076b629
Lihuor Slot <lihuor@gmail.com>
2023-08-15 19:54:27 +1000

    first init

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -69,7 +69,7 @@ export default function Form() {
         <div className='input-control'>
             <textarea name='description' value={description} id='description' cols="30" rows="4" onChange={handleInput('description')} placeholder='Description'></textarea>
         </div>
-        <div className='sumbit-btn'>
+        <div className='submit-btn'>
             <button>Add Income</button>
         </div>
     </Formstyled>
@@ -77,6 +77,46 @@ export default function Form() {
 }
 
 const Formstyled = styled.form`
-
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    input, textarea, select {
+        font-family: inherit;
+        font-size: inherit;
+        outline: none;
+        border: none;
+        padding: .5rem 1rem;
+        border-radius: .5rem;
+        border: 2px solid #fff;
+        resize: none;
+        box-shadow: 0px 1px 15px rgba(0,0,0,0.06);
+        color: rgba(34, 34, 96, .9);
+        &::placeholder {
+            color: rgba(34, 34, 96, .4);
+        }
+    }
+    .input-control {
+        input {
+            width: 100%;
+        }
+    }
+    .select-input-control {
+        display: flex;
+        justify-content: flex-end;
+        select {
+            color: rgba(34, 34, 96, .4);
+            &:focus, &:active {
+                color: rgba(34, 34, 96, 1);
+            }
+        }
+    }
+    .submit-btn {
+        button {
+            box-shadow: 0px 1px 15px rgba(0,0,0,0.06);
+            &:hover{
+                background: var(--color-green) !important;
+            }
+        }
+    }
 `;
 
